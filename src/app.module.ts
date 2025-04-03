cf1215f350a8577193c90169bdf2e07257ae80df
Alan Christian Ramírez Rodríguez <ingenieroacramirez@gmail.com>
2025-04-02 19:53:44 -0600

    agregar módulo, controlador y servicio para gestionar nombres con pruebas unitarias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NamesModule } from './names/names.module';
 
 @Module({
-  imports: [],
+  imports: [NamesModule],
   controllers: [AppController],
   providers: [AppService],
 })
